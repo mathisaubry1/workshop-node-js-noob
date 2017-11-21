ec4b8aa9a3ecb2ed4fcc0a1ab1447c80e234e69d
mathisaubry1 <mathis.a.aubry@gmail.com>
2017-11-21 16:31:10 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,7 +2,16 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  function repeat (nb_repeat, string)
+  {
+    var repeated_string = string;
 
+    for (i = 1; i < nb_repeat; i++)
+    {
+      repeated_string = repeated_string + ", " + string;
+    }
+    return (repeated_string);
+  }
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
   /** */
