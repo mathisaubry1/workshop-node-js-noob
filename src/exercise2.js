9fa36ccc993e7eae877523803b3ea01c3cca52bf
mathisaubry1 <mathis.a.aubry@gmail.com>
2017-11-21 16:01:15 +0100

    Exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,13 @@
-exports.run = function(input) {
-  console.log("Exercise #2");
+exports.run = function(input)
+{
+  var max = 0;  
+  input.split("");
+  for (i = 0; input[i]; i++)
+  {
+    if (input[i] > max)
+    {
+      max = parseInt(input[i]);
+    }
+  }
+  return (max);
 };
