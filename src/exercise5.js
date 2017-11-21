9f580377b11972873bb2fd3cc67c2f22c5ec49d8
mathisaubry1 <mathis.a.aubry@gmail.com>
2017-11-21 16:54:04 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,15 @@
-exports.run = function(input) {
-  console.log("Exercise #5");
+exports.run = function(input)
+{
+  var fs = require("fs");
+  
+  fs.readdir(input.directory, function callback (err, list)
+  {
+    if (err)
+    {
+      input.showError;
+    } else
+    {
+      input.showDone (list.length);
+    }
+  });
 };
