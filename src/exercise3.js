c07e0c56723192a515c06d37c698e79e365a8a1f
mathisaubry1 <mathis.a.aubry@gmail.com>
2017-11-21 16:19:36 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,21 @@
-exports.run = function(input) {
-  console.log("Exercise #3");
+exports.run = function(input)
+{
+  var total = 1;
+  var all_false = true;
+
+  for (i = 0; input[i]; i++)
+  {
+    if (input[i].multiply === true)
+    {
+      total = total * input[i].value;
+      all_false = false;
+    }
+  }
+  if (all_false === true)
+  {
+    return (0);
+  } else
+  {
+    return (total);
+  }
 };
